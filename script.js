17b05270d9167a9ab0f559846e1f5809b2d213c4
Ankit <Ankitojha121212@gmail.com>
2024-09-10 21:48:34 +0530

    UI DONE

---
+++ script.js
@@ -0,0 +1,10 @@
+// Simulating an incoming message
+document.querySelector('.notification-bell').addEventListener('click', () => {
+    let notificationCount = document.querySelector('.notification-count');
+    let currentCount = parseInt(notificationCount.textContent);
+
+    // Increment count and display
+    currentCount += 1;
+    notificationCount.textContent = currentCount;
+    document.querySelector('.notification-bell').classList.add('has-new');
+});
